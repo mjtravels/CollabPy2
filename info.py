485af0c0496ce1021a09560974db373dbb6c7410
mjtravels <47067733+mjtravels@users.noreply.github.com>
2019-03-24 20:39:34 -0400

    Update info.py
    change indenting only

--- info.py
+++ info.py
@@ -2,6 +2,13 @@
 # this would be saved locally with each user's credentials and added to gitignore
 
 logins = {
-  'meetup': {'api_key': 'example_api_key', 'oauth_1': 'example_oauth_thing'},
-	'source2': {'api_key': 'example_api_key', 'username': 'example_username', 'password': 'example_password' }
+  'meetup': {
+	  'api_key': 'example_api_key',
+	  'oauth_1': 'example_oauth_thing'
+  },
+  'source2': {
+	  'api_key': 'example_api_key',
+	  'username': 'example_username',
+	  'password': 'example_password'
+  }
 }
