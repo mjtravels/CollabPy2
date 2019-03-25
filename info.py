1b232abf076c5b9811196c1756dd8af140c88495
mjtravels <47067733+mjtravels@users.noreply.github.com>
2019-03-24 20:09:44 -0400

    Create info.py
    converting into.r to python dict - dummy data

---
+++ info.py
@@ -0,0 +1,7 @@
+# Converting info.py to python dict
+# this would be saved locally with each user's credentials and added to gitignore
+
+logins = {
+  'meetup': {'api': 'example_api_key', 'oauth_1': 'example_oauth_thing'},
+	'source2': {'api': 'example_api', 'username': 'example_username', 'password': 'example_password' }
+}
