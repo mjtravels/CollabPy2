036b974fbfdc4da23b42d20f0c15c7cea20314a5
mjtravels <47067733+mjtravels@users.noreply.github.com>
2019-03-24 20:37:41 -0400

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@
 # this would be saved locally with each user's credentials and added to gitignore
 
 logins = {
-  'meetup': {'api': 'example_api_key', 'oauth_1': 'example_oauth_thing'},
-	'source2': {'api': 'example_api', 'username': 'example_username', 'password': 'example_password' }
+  'meetup': {'api_key': 'example_api_key', 'oauth_1': 'example_oauth_thing'},
+	'source2': {'api_key': 'example_api_key', 'username': 'example_username', 'password': 'example_password' }
 }
