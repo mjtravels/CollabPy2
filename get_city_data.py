8af751f4e2c5a5bf28dbcb64c82dcd1e4c0d085b
Caiqian Cropper <caiqian.wu@gmail.com>
2019-03-26 18:38:10 -0500

    added loop for requesting pdx api responses for multiple categories

--- get_city_data.py
+++ get_city_data.py
@@ -12,6 +12,8 @@ import pandas as pd
 from pandas.io.json import json_normalize
 import numpy as np
 
+mj_categories = [1,4,5,6,9,12,13,14,23,26,27,30,31,32,34]
+
 # access API key saved in a api_key.txt file locally
 # api_file = open("api_key.txt", "r")
 # api_key = api_file.read()
@@ -28,6 +30,12 @@ group_info = client.GetGroup({'urlname':'Houston_PyLadies'})
 pdx_request = requests.get("https://api.meetup.com/find/groups?zip=97215&key="+api_key)
 # limit request to 1 category in 1 zip code
 cat1_pdx_request = requests.get("https://api.meetup.com/find/groups?zip=97215&category_id=1&key="+api_key).content
+
+# make an API call for each category MJ is interested in; store API responses in list
+cats_pdx_requests = []
+for cat in mj_categories:
+    cats_pdx_requests.append(requests.get("https://api.meetup.com/find/groups?zip=97215&category_id="+str(cat)+"&key="+api_key).content)
+
 
 # alternate way perform request, using params= as argument for requests.get()
 # seems to return a 'Response' type
