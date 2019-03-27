f4d23d72c53277f391b6269b97baaf175ba00072
Caiqian Cropper <caiqian.wu@gmail.com>
2019-03-27 13:13:57 -0500

    fixed API request category_id to category
    git status

--- get_city_data.py
+++ get_city_data.py
@@ -12,6 +12,7 @@ import pandas as pd
 from pandas.io.json import json_normalize
 
 mj_categories = [1,4,5,6,9,12,13,14,23,26,27,30,31,32,34]
+example_mj_cats = [1,4,5]
 
 # access API key saved in a api_key.txt file locally
 # api_file = open("api_key.txt", "r")
@@ -27,18 +28,20 @@ group_info = client.GetGroup({'urlname':'Houston_PyLadies'})
 
 # search by zip code directly - using requests
 pdx_request = requests.get("https://api.meetup.com/find/groups?zip=97215&key="+api_key)
-# limit request to 1 category in 1 zip code
-cat1_pdx_request = requests.get("https://api.meetup.com/find/groups?zip=97215&category_id=1&key="+api_key).content
+# limit request to 3 categories in 1 zip code as example
+cat1_pdx_request = requests.get("https://api.meetup.com/find/groups?zip=97215&category=1,4,5&key="+api_key).content
 
 # make an API call for each category MJ is interested in; store API responses in list
+# need to revise because New Age & Sprituality shouldn't be a result
 cats_pdx_requests = []
 for cat in mj_categories:
-    cats_pdx_requests.append(requests.get("https://api.meetup.com/find/groups?zip=97215&category_id="+str(cat)+"&key="+api_key).content)
+    cats_pdx_requests.append(requests.get("https://api.meetup.com/find/groups?zip=97215&category="+str(cat)+"&key="+api_key).content)
 
 
+# resource: https://www.dataquest.io/blog/python-api-tutorial/
 # alternate way perform request, using params= as argument for requests.get()
 # seems to return a 'Response' type
-response2 = requests.get("http://api.meetup.com/find/groups", params={'city':'Portland','category_id':1,'key':api_key})
+response2 = requests.get("http://api.meetup.com/find/groups", params={'city':'Portland','category':34,'key':api_key})
 
 # normalize json data
 # df = pd.DataFrame.from_dict(json_normalize(pdx_request), orient='columns')
